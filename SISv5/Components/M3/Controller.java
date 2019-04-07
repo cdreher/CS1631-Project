21c32f676e8ae2fcffb4c8564d845b7479609134
Tianrun Gu <kyrie@Tianruns-MBP.fios-router.home>
2019-04-07 00:20:28 -0400

    fixed send and load

--- SISv5/Components/M3/Controller.java
+++ SISv5/Components/M3/Controller.java
@@ -115,13 +115,16 @@ public class Controller {
     @FXML
     private TextArea infoArea;
 
-    private static ObservableList<Message> data = FXCollections
+    @FXML
+    private Stage primaryStage;
+
+    private ObservableList<Message> data = FXCollections
             .observableArrayList();
 
-    private static ObservableList<Field> toSendData = FXCollections
+    private ObservableList<Field> toSendData = FXCollections
             .observableArrayList();
 
-    private static ObservableList<Field> receivedData = FXCollections
+    private ObservableList<Field> receivedData = FXCollections
             .observableArrayList();
 
     private Dialog<String> dialog = new Dialog<>();
@@ -144,6 +147,8 @@ public class Controller {
     private String SCOPE = "SIS.Scope1";
 
     private int port = 53217;
+
+    private DebuggerProc proc;
 
     private boolean runProcessMsg;
 
@@ -183,9 +188,6 @@ public class Controller {
             conn.putPair("Name", NAME);
             encoder.sendMsg(conn);
 
-            //print out the connect message
-            information.appendText("\nSuccessfully Connected!");
-
         } catch (Exception e) {
             //print the error message
             e.printStackTrace();
@@ -224,54 +226,65 @@ public class Controller {
         String passcode = "password";
         String admin_user = "admin";
         String admin_passcode = "1234";
-        userTable.put(user1,passcode);
+        userTable.put(user1, passcode);
         userTable.put(admin_user, admin_passcode);
 
         information.setText("Checking the passcode...");
 
-        if (user.equals(admin_user)) {
-          //if the password is correct, connect to the server
-          information.appendText("\nConnecting to server....");
-          connectToServer();
-          Stage window;
-          window = (Stage) logButton.getScene().getWindow();
-          Parent root = FXMLLoader.load(getClass().getResource("Admin.fxml"));
-          Scene voteScene = new Scene(root,950,600);
-          window.setScene(voteScene);
-          window.setTitle("Welcome to Voting System");
+        Stage window = (Stage) logButton.getScene().getWindow();
+        primaryStage = window;
+        information.appendText("\nConnecting to server....");
 
-          //we display the GUI
-          window.show();
+        if (user.equals(admin_user)) {
+            proc = new DebuggerProc();
+            //if the password is correct, connect to the server
+            connectToServer();
+
+            //print out the connect message
+            information.appendText("\nSuccessfully Connected!");
+            Parent root = FXMLLoader.load(getClass().getResource("Vote.fxml"));
+            Scene voteScene = new Scene(root, 950, 600);
+            window.setScene(voteScene);
+            window.setTitle("Welcome to Voting System");
+
+            //we display the GUI
+            window.show();
         } else {
             if (pass.equals(userTable.get(user))) {
-              //if the password is correct, connect to the server
-              information.appendText("\nConnecting to server....");
-              connectToServer();
-              Stage window;
-              window = (Stage) logButton.getScene().getWindow();
-              Parent root = FXMLLoader.load(getClass().getResource("Vote.fxml"));
-              Scene voteScene = new Scene(root,950,600);
-              window.setScene(voteScene);
-              window.setTitle("Welcome to Voting System");
+                proc = new DebuggerProc();
+                //if the password is correct, connect to the server
+                connectToServer();
 
-              //we display the GUI
-              window.show();
+                //print out the connect message
+                information.appendText("\nSuccessfully Connected!");
+                Parent root = FXMLLoader.load(getClass().getResource("Vote.fxml"));
+                Scene voteScene = new Scene(root, 950, 600);
+                window.setScene(voteScene);
+                window.setTitle("Welcome to Voting System");
 
+                //we display the GUI
+                window.show();
             } else {
                 information.appendText("\nInvalid user");
             }
         }
 
+
     }
 
+    @FXML
     public void handlerClear() {
         data.clear();
     }
 
-    public void handlerSend() {
+    @FXML
+    public void handlerSend() throws Exception {
+
+        connectToServer();
+
         ObservableList<Message> selectedItems = messages.getSelectionModel()
                 .getSelectedItems();
-        // System.out.println(selectedItems.size());
+        //System.out.println(selectedItems.size());
         for (Message message : selectedItems) {
             String path = message.getPath();
 
@@ -279,19 +292,74 @@ public class Controller {
                 KeyValueList kvList = XMLUtil.extractToKV(new File(path));
                 encoder.sendMsg(kvList);
             } catch (Exception e) {
-                // TODO Auto-generated catch block
-                // e.printStackTrace();
-                // data.remove(message);
-                dialog.setTitle("Error");
-                dialog.setHeaderText("Fail to send message");
-                dialog.setContentText(message.getName() + "\n\n"
-                        + message.getPath());
-                dialog.showAndWait();
+                e.printStackTrace();
             }
         }
     }
 
+    @FXML
     public void handlerLoad() {
+        connectToServer();
+        //fake constructor
+        try {
+            messages.setItems(data);
+            messages.setCellFactory(new Callback<ListView<Message>, ListCell<Message>>() {
+
+                public ListCell<Message> call(ListView<Message> param) {
+                    final ListCell<Message> cell = new ListCell<Message>() {
+                        @Override
+                        public void updateItem(Message item, boolean empty) {
+                            super.updateItem(item, empty);
+                            if (item != null) {
+                                setText(item.getName());
+                                setTooltip(new Tooltip(item.getPath()));
+                            }
+                        }
+                    }; // ListCell
+                    return cell;
+                }
+            }); // setCellFactory
+
+            messages.getSelectionModel().selectedItemProperty()
+                    .addListener(new ChangeListener<Message>() {
+                        public void changed(
+                                ObservableValue<? extends Message> observable,
+                                Message oldValue, Message newValue) {
+                            toSendData.clear();
+
+                            try {
+                                if (observable != null
+                                        && observable.getValue() != null) {
+                                    String path = observable.getValue()
+                                            .getPath();
+                                    KeyValueList kvList = XMLUtil
+                                            .extractToKV(new File(path));
+
+                                    List<String> table = kvList.tableOutput();
+                                    for (String string : table) {
+                                        String[] parts = string.split(" ");
+                                        toSendData.add(new Field(parts[0],
+                                                parts[1]));
+                                    }
+                                }
+
+                            } catch (Exception e) {
+                                // TODO Auto-generated catch block
+                                // data.remove(newValue);
+                                // messages.setItems(null);
+                                // messages.setItems(data);
+                            }
+                        }
+                    });
+
+            messages.getSelectionModel().setSelectionMode(
+                    SelectionMode.MULTIPLE);
+        } catch (Exception exception) {
+            throw new RuntimeException(exception);
+        }
+        //end of fake constructor
+
+
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Load XML messages / Script");
         List<String> exts = new ArrayList<String>() {
@@ -309,7 +377,6 @@ public class Controller {
                 .setInitialDirectory(new File(System.getProperty("user.dir")));
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("XML, TXT", exts));
-        Stage primaryStage = (Stage) castButton.getScene().getWindow();
         List<File> files = fileChooser.showOpenMultipleDialog(primaryStage);
 
         if (files != null) {
@@ -338,6 +405,7 @@ public class Controller {
         }
     }
 
+    @FXML
     private void loadOneXML(File file, File base) {
         if (!file.exists()) {
             String prefix = base.getParent();
@@ -353,106 +421,144 @@ public class Controller {
         }
     }
 
+    @FXML
     public void handlerMessagesHint() {
         messagesTitle.setText("Hold 'Ctrl' To Select");
     }
 
+    @FXML
     public void handlerMessagesHintExit() {
         messagesTitle.setText("Message(s)");
     }
 
     @FXML
     public void handlerTerminate() throws Exception {
-      // try to establish a connection to SISServer
-      universal = connect();
-
-      //bind the message writer to outputstream of the socket
-      encoder = new MsgEncoder(universal.getOutputStream());
-
-
-      KeyValueList reg = new KeyValueList();
-
-      reg.putPair("Scope", "SIS.Scope1");
-      reg.putPair("MessageType", "25");
-      reg.putPair("Passcode", "1234");
-      reg.putPair("SecurityLevel", "3");
-      reg.putPair("Sender", "SISServer");
-      reg.putPair("Receiver", "Compo");
-      encoder.sendMsg(reg);
-
-      infoArea.appendText("Voting has been terminated! Awaiting results...\n");
+        connectToServer();
+        infoArea.appendText("Voting has been terminated! Awaiting results...\n");
     }
 
     @FXML
-    public void handlerVote() throws Exception{
-      // try to establish a connection to SISServer
-      universal = connect();
+    public void handlerVote() throws Exception {
+        connectToServer();
+        KeyValueList reg = new KeyValueList();
 
-      // bind the message reader to inputstream of the socket
-      decoder = new MsgDecoder(universal.getInputStream());
+        reg.putPair("Scope", "SIS.Scope1");
+        reg.putPair("MessageType", "701");
+        reg.putPair("VoterPhoneNo", phoneNum.getText());
+        reg.putPair("CandidateID", candID.getText());
+        reg.putPair("Sender", "SISServer");
+        reg.putPair("Receiver", "Compo");
+        encoder.sendMsg(reg);
 
-      //bind the message writer to outputstream of the socket
-      encoder = new MsgEncoder(universal.getOutputStream());
-
-      runProcessMsg = true;
-
-      KeyValueList conn = new KeyValueList();
-      conn.putPair("Scope", SCOPE);
-      conn.putPair("MessageType", "Register");
-      conn.putPair("Role","Basic");
-      conn.putPair("Name", NAME);
-      encoder.sendMsg(conn);
-
-      conn = new KeyValueList();
-      conn.putPair("Scope", SCOPE);
-      conn.putPair("MessageType", "Connect");
-      conn.putPair("Role","Basic");
-      conn.putPair("Name", NAME);
-      encoder.sendMsg(conn);
-
-      KeyValueList reg = new KeyValueList();
-
-      reg.putPair("Scope", "SIS.Scope1");
-      reg.putPair("MessageType", "701");
-      reg.putPair("VoterPhoneNo", phoneNum.getText());
-      reg.putPair("CandidateID", candID.getText());
-      reg.putPair("Sender", "SISServer");
-      reg.putPair("Receiver", "Compo");
-      encoder.sendMsg(reg);
-
-      KeyValueList kvList;
-      while(runProcessMsg){
-          // attempt to read and decode a message, see MsgDecoder for details
-          try {
-            kvList = decoder.getMsg();
-            //process that message
-            ProcessMsg(kvList);
-          } catch (Exception e){
-              e.printStackTrace();
-          }
-      }
+        KeyValueList kvList;
+        while (runProcessMsg) {
+            // attempt to read and decode a message, see MsgDecoder for details
+            try {
+                kvList = decoder.getMsg();
+                //process that message
+                ProcessMsg(kvList);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    private void ProcessMsg(KeyValueList kvList) throws Exception{
+    class DebuggerProc {
+        private Thread thread;
+        private DebuggerTask debuggerTask;
+
+        public DebuggerProc() {
+            debuggerTask = new DebuggerTask();
+            thread = new Thread(debuggerTask);
+            thread.start();
+        }
+
+        public void pause() {
+            debuggerTask.pause();
+        }
+
+        public void resume() {
+            debuggerTask.resume();
+        }
+
+        public void close() {
+            try {
+                debuggerTask.terminate();
+                thread.join();
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+    }
+
+    class DebuggerTask extends Task<Void> {
+
+        private volatile boolean running = true;
+        private volatile boolean pause = false;
+
+        public void terminate() throws Exception {
+            // TODO Auto-generated method stub
+            universal.shutdownInput();
+            universal.shutdownOutput();
+            universal.close();
+            running = false;
+        }
+
+        public void pause() {
+            pause = true;
+        }
+
+        public void resume() {
+            pause = false;
+        }
+
+        @Override
+        protected Void call() throws Exception {
+            // TODO Auto-generated method stub
+            while (running) {
+                if (!pause) {
+                    try {
+                        KeyValueList msg = decoder.getMsg();
+                        if (msg != null) {
+                            receivedData.clear();
+                            List<String> table = msg.tableOutput();
+                            for (String string : table) {
+                                String[] parts = string.split(" ");
+                                receivedData.add(new Field(parts[0], parts[1]));
+                            }
+                        }
+                        Thread.sleep(3000);
+                        // TABLEVIEW
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        // e.printStackTrace();
+                        break;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+
+    private void ProcessMsg(KeyValueList kvList) throws Exception {
 
         String sender = kvList.getValue("Sender");
         String type = kvList.getValue("MessageType");
         String receiver = kvList.getValue("Receiver");
 
-        switch(type){
+        switch (type) {
             case "711":
-              String status = kvList.getValue("Status");
-              if(status.equals("2")){
-                infoArea.appendText("Invalid candidate. Vote is not counted.\n");
-              }
-              else if(status.equals("1")){
-                infoArea.appendText("Vote can not be counted. This user has voted already.\n");
-              }
-              else {
-                infoArea.appendText("Your vote has been cast!\n");
-              }
-              runProcessMsg = false;
-              break;
+                String status = kvList.getValue("Status");
+                if (status.equals("2")) {
+                    infoArea.appendText("Invalid candidate. Vote is not counted.\n");
+                } else if (status.equals("1")) {
+                    infoArea.appendText("Vote can not be counted. This user has voted already.\n");
+                } else {
+                    infoArea.appendText("Your vote has been cast!\n");
+                }
+                runProcessMsg = false;
+                break;
 
             // case "21":
             //     if(adminPassword.equals(kvList.getValue("Passcode")) && securityLevel.equals(kvList.getValue("SecurityLevel"))){
